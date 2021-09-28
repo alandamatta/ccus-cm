1ad84543a01ed1f0fe4a1a9e609fc4bffbf082ad
Alan <alandamatta@live.com>
2021-09-28 10:03:44 -0500

    time fix

--- resources/js/timesheet.js
+++ resources/js/timesheet.js
@@ -3,12 +3,15 @@ import { generateTimesheetTable } from './components/timesheetTable'
 import { ajaxGet } from './ajaxUtils'
 
 $('.checkInButton').on('click', function (event) {
-  // event.preventDefault()
+  event.preventDefault()
+  const element = $(this)
+  const form = element.closest('form')
+  form.find('[name=time]').val(new Date())
   $(this).addClass('is-loading')
+  form.submit()
 })
 
 $('.cancelCheckInButton').on('click', function (event) {
-  // event.preventDefault()
   const element = $(this)
   element.find('span').addClass('is-hidden')
   element.addClass('is-loading')

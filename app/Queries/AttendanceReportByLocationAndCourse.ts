16f53a9c4c04d61fefa2ddfbc560c3722f45e6b5
Alan <alandamatta@live.com>
2022-05-02 15:19:23 -0500

    attendance report fix

--- app/Queries/AttendanceReportByLocationAndCourse.ts
+++ app/Queries/AttendanceReportByLocationAndCourse.ts
@@ -1,26 +1,19 @@
 export default function attendanceReportByLocationAndCourse() {
   return `
-SELECT
-    s.full_name studentName,
-    l.name as locationName,
-    c.name as courseName,
-    CONCAT(COUNT(sc.student_id), '(',
-        FORMAT(ABS(FLOOR(100 * COUNT(sc.student_id)) / FLOOR(ABS(DATEDIFF(GREATEST(:startDate, sc.created_at), :endDate)) / 7)), 0),
-        '%) ') present,
-    (COUNT(sc.student_id) - FLOOR(ABS(DATEDIFF(GREATEST(:startDate, sc.created_at), :endDate)) / 7)) absent
-FROM attendances a
-        INNER JOIN students_courses sc ON sc.student_id = a.student_id AND sc.course_id = a.course_id
-        INNER JOIN courses c on sc.course_id = c.id
-        INNER JOIN students s on sc.student_id = s.id
-        INNER JOIN day_of_week dow on c.day_of_week = dow.value
-        INNER JOIN locations l on c.location_id = l.id
-WHERE check_in IS TRUE AND
-      (c.location_id = :locationId || :locationId < 0) AND
+SELECT s.id, s.full_name studentName, c.name courseName, l.name locationName, CEIL(COALESCE(count(a.id), 0) / 2) as present,
+FORMAT(ABS(CEIL(ABS(DATEDIFF(GREATEST(:startDate, sc.created_at), :endDate)) / 7) - (COUNT(a.id) / 2)), 0) absent,
+CONCAT(FORMAT(ABS(FLOOR(100 * (COUNT(a.id) / 2)) / (FLOOR(ABS(DATEDIFF(GREATEST(:startDate, sc.created_at), :endDate)) / 7) + 1)), 0), '%') frequency,
+DATE_FORMAT(sc.created_at, '%m/%d/%Y') studentStartDate FROM
+students_courses sc
+LEFT JOIN attendances a on sc.student_id = a.student_id AND sc.course_id = a.course_id AND DATE(a.time) BETWEEN DATE(:startDate) AND DATE(:endDate)
+INNER JOIN students s on sc.student_id = s.id
+INNER JOIN courses c on sc.course_id = c.id
+INNER JOIN locations l on c.location_id = l.id
+WHERE (l.id = :locationId || :locationId < 0) AND
       (c.id = :courseId || :courseId < 0) AND
       s.disabled_at IS NULL AND
-      DATE(a.time) BETWEEN DATE(:startDate) AND DATE(:endDate) AND
       (:admin IS TRUE || c.location_id = :userLocation)
-GROUP BY s.id, s.full_name, l.name, c.name, sc.id
+GROUP BY sc.id, c.id, c.name, s.full_name
 ORDER BY s.full_name;
 
 `
